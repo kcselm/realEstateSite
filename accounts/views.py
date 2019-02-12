776b266990d1414184cb1bb9895af114af86139d
kcselm <kcselm93@gmail.com>
2019-02-11 21:47:01 -0600

    Setting up validation for the register page and registering new user

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages 
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request): 
     if request.method == 'POST':
-        #Register user
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        #Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #Check if passwords match
+        if password == password2:
+            #check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Oop, that username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Oop, that email is being used')
+                    return redirect('register')
+                else:
+                    #Everything okay
+                    user = User.objects.create_user(username=username, password=password, 
+                    email=email, first_name=first_name, last_name=last_name)
+                    # --Logged after register--
+                    auth.login(request, user)
+                    messages.success(request, 'You are now logged in')
+                    return redirect('index')
+                    #--Register then login--
+                    # user.save()
+                    # messages.success(request, 'You are now registered and can log in')
+                    # return redirect('login')
+        else:
+            messages.error(request, 'Oop, passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
