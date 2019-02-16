9356345d5b2a4eba02e9b2a2f0bee49ff319cf91
kcselm <kcselm93@gmail.com>
2019-02-16 12:43:23 -0600

    Setting email up to work in the future

--- realEstate/settings.py
+++ realEstate/settings.py
@@ -141,3 +141,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# #Email config
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_POST = 587
+# EMAIL_HOST_USER = 'email@gmail.com'
+# EMAIL_HOST_PASSWORD=''
+# EMAIL_USE_TLS=True
